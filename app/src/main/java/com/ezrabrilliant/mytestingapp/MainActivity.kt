34bf325f564e304709549f6c7df4b5ef310203b5
Ezra <c14220315@john.petra.ac.id>
2024-10-02 04:03:00 +0700

    adding ScrollView, etc

--- app/src/main/java/com/ezrabrilliant/mytestingapp/MainActivity.kt
+++ app/src/main/java/com/ezrabrilliant/mytestingapp/MainActivity.kt
@@ -1,42 +1,15 @@
 package com.ezrabrilliant.mytestingapp
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ezrabrilliant.mytestingapp.R
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var btnSetValue: Button
-    private lateinit var tvText: TextView
-
-    private var names = ArrayList<String>()
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        tvText = findViewById(R.id.tv_text)
-        btnSetValue = findViewById(R.id.btn_set_value)
-
-        btnSetValue.setOnClickListener(this)
-
-        names.add("Ezra")
-        names.add("Sat")
-        names.add("Rick")
-    }
-
-    override fun onClick(view: View) {
-        if (view.id == R.id.btn_set_value) {
-            Log.d("MainActivity", names.toString())
-            val name = StringBuilder()
-            for (i in 0..2) {
-                name.append(names[i]).append("\n")
-            }
-            tvText.text = name.toString()
-        }
     }
 }
